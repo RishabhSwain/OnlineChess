769060cb35d41408930b0b7b65d2ca2eefc16366
RishabhSwain <105536044+RishabhSwain@users.noreply.github.com>
2026-03-18 15:23:14 +0530

    auth middleware changes

--- frontend/middleware.ts
+++ frontend/middleware.ts
@@ -23,5 +23,5 @@ export function middleware(req: NextRequest) {
 }
 
 export const config = {
-  matcher: ["/menu/:path*", "/"], // paths to protect
+  matcher: ["/menu/:path*"], // paths to protect
 };
